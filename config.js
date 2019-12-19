405850a78cd856572462bf37475c6e6ca24f4d08
Derviş Kayımbaşıoğlu <simonarez@hotmail.com>
2019-12-19 14:59:14 +0200

    initial configuration

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAlNsuMxcgsBAEshwOsLUw0cg7w86JBILnGhUHWvIteE5zQFPFUL2ISqceCgJ9j0PRvf0K1GoTWdzAPxcf9xTe5S0wd9wUweCItqwCC3WsYZB3upU6JvoVFw3csutOxHIlyVqbhv2vS3uZALX4bg5jrzCZC5VJJd3bekKJnePuZA0h39SkDeJMXl7HEjHgQZD',
-    FB_VERIFY_TOKEN: 'my_voice_bot_sex_on_the_beach_every_day',
-    FB_APP_SECRET: '500a2471e6b08f6edf502e2735c31471',
-    SERVER_URL: 'https://safe-woodland-87361.herokuapp.com/',
-    GOOGLE_PROJECT_ID: '',
-    DF_LANGUAGE_CODE: '',
-    GOOGLE_CLIENT_EMAIL: '',
-    GOOGLE_PRIVATE_KEY: ''
+    FB_PAGE_TOKEN: process.env.FB_PAGE_TOKEN,
+    FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
+    FB_APP_SECRET: process.env.FB_APP_SECRET,
+    SERVER_URL: process.env.SERVER_URL,
+    GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
+    DF_LANGUAGE_CODE: process.env.DF_LANGUAGE_CODE,
+    GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
+    GOOGLE_PRIVATE_KEY: process.env.GOOGLE_PRIVATE_KEY
 };
