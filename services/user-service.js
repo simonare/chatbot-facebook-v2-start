416efbac5388944aec038bc908ea99e5c3b4b08b
Derviş Kayımbaşıoğlu <simonarez@hotmail.com>
2019-12-26 09:35:58 +0200

    'refactoring'

--- services/user-service.js
+++ services/user-service.js
@@ -20,7 +20,6 @@ module.exports = {
                 if (user.first_name){
                     console.log("FB user: %s %s %s %s %s %s", user.first_name, user.last_name, user.profile_pic, user.locale, user.timezone, user.gender);
                     await storeUserData(senderID, user);
-                    done();
                     callback(user);
                 }
                 else{
