d5d4a01dd40b5299bc60eadb3535a152a464eea7
Derviş Kayımbaşıoğlu <simonarez@hotmail.com>
2020-01-31 11:49:23 +0200

    'commit'

--- routes/webviews.js
+++ routes/webviews.js
@@ -6,6 +6,9 @@ const fbService = require('../services/fb-service');
 
 const router = express.Router();
 
+const pg = require('pg');
+pg.defaults.ssl = true;
+
 router.get('/webview', function(req, res){
     res.render('newsletter-settings');
 });
@@ -13,9 +16,26 @@ router.get('/webview', function(req, res){
 router.get('/save', function(req, res){
     let body = req.query;
     let topics = body.topics.join(',');
-    let response = `Newsletter ${body.newsletter}, topics: ${topics} and deals ${body.deals} for psid ${body.psid}`
-
-    fbService.sendTextMessage(body.psid, response);
+    let pool = new pg.Pool(config.PG_CONFIG);
+    pool.connect(function(err, client, done){
+        if (err){
+            return console.error("Error acquiering client");
+        }
+        client.query("UPDATE public.users SET newsletter=$1, topics=$2, deals=$3 WHERE fb_id=$4",
+            [
+                body.newsletter,
+                topics,
+                body.deals,
+                body.psid
+            ],
+            function(err, result){
+                if (err === null){
+                    fbService.sendTextMessage(body.psid, 'Settings saved.');
+                } else {
+                    console.log('ERR: ' + err);
+                }
+            });
+    });
 });
 
 
