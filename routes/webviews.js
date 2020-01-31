8e1681543ca627c3730331d0e01c61601470174c
Derviş Kayımbaşıoğlu <simonarez@hotmail.com>
2020-01-31 08:56:04 +0200

    'commit'

--- routes/webviews.js
+++ routes/webviews.js
@@ -6,11 +6,11 @@ const fbService = require('../services/fb-service');
 
 const router = express.Router();
 
-router.get('webview', function(req, res){
+router.get('/webview', function(req, res){
     res.render('newsletter-settings');
 });
 
-router.get('save', function(req, res){
+router.get('/save', function(req, res){
     let body = req.query;
     let topics = body.topics.join(',');
     let response = `Newsletter ${body.newsletter}, topics: ${topics} and deals ${body.deals} for psid ${body.psid}`
